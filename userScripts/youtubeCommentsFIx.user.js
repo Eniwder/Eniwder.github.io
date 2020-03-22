205ae33c0d0778783b0adfa075c2d7f4bba5f2a8
Eniwder <akabudou0120@yahoo.co.jp>
2020-03-22 09:50:28 +0900

    improve userscript

--- userScripts/youtubeCommentsFIx.user.js
+++ userScripts/youtubeCommentsFIx.user.js
@@ -6,42 +6,71 @@
 // @match    https://www.youtube.com/watch*
 // ==/UserScript==
 
-setTimeout(function () {
-
+function setPlayerSize() {
   window.eval(
-    `_yt_player.Ud = function(a,b){  
-    this.width = Math.min(a,722);
-    this.height = Math.min(b,408); 
-  }
-  _yt_player.Ud.prototype.clone = function() {
-    return new _yt_player.Ud(this.width,this.height)
-  }
-  
-  _yt_player.Ud.prototype.aspectRatio = function() {
-    return this.width / this.height
-  }
-  
-  _yt_player.Ud.prototype.isEmpty = function() {
-    return !(this.width * this.height)
-  }
-  
-  _yt_player.Ud.prototype.ceil = function() {
-    this.width = Math.ceil(this.width);
-    this.height = Math.ceil(this.height);
-    return this
-  }
-  
-  _yt_player.Ud.prototype.floor = function() {
-    this.width = Math.floor(this.width);
-    this.height = Math.floor(this.height);
-    return this
-  }
-  
-  _yt_player.Ud.prototype.round = function() {
-    this.width = Math.round(this.width);
-    this.height = Math.round(this.height);
-    return this
-  }
-  `);
+    `
+_yt_player.Ud = function(a,b){  
+  this.width = Math.min(a,722);
+  this.height = Math.min(b,408); 
+}
+_yt_player.Ud.prototype.clone = function() {
+  return new _yt_player.Ud(this.width,this.height)
+}
 
-}, 200);
+_yt_player.Ud.prototype.aspectRatio = function() {
+  return this.width / this.height
+}
+
+_yt_player.Ud.prototype.isEmpty = function() {
+  return !(this.width * this.height)
+}
+
+_yt_player.Ud.prototype.ceil = function() {
+  this.width = Math.ceil(this.width);
+  this.height = Math.ceil(this.height);
+  return this
+}
+
+_yt_player.Ud.prototype.floor = function() {
+  this.width = Math.floor(this.width);
+  this.height = Math.floor(this.height);
+  return this
+}
+
+_yt_player.Ud.prototype.round = function() {
+  this.width = Math.round(this.width);
+  this.height = Math.round(this.height);
+  return this
+}
+
+window.dispatchEvent(new Event("resize"));
+
+`);
+
+}
+
+function helper(elem, func) {
+  if (!window.eval(elem)) {
+    return setTimeout(() => helper(elem, func), 100);
+  }
+  func();
+}
+
+
+helper("_yt_player.Ud", setPlayerSize);
+helper("document.getElementById('comments')", autoLoadComments);
+
+// オマケでコメント追加読み込みの自動化
+function autoLoadComments() {
+  let bestm = 0;
+  document.getElementById('comments').addEventListener('scroll', e => {
+    if (e.target.scrollTop > e.target.scrollTopMax - 100) {
+      if (bestm != e.target.scrollTopMax) {
+        window.eval(`document.querySelector('yt-visibility-monitor').dirty_ = true;`);
+        bestm = e.target.scrollTopMax;
+        window.eval(`document.querySelector('yt-visibility-monitor').refreshVisibility_.bind(document.querySelector('yt-visibility-monitor'))();`);
+      }
+    }
+  });
+}
+
