9eed98bcd5c383f1db24512a4b4505cfebf15dad
Eniwder <akabudou0120@yahoo.co.jp>
2020-03-23 05:02:35 +0900

    'userscript修正'

--- userScripts/youtubeCommentsFIx.user.js
+++ userScripts/youtubeCommentsFIx.user.js
@@ -3,12 +3,10 @@
 // @version  1
 // @author https://twitter.com/Eniel120
 // @grant    none
-// @match    https://www.youtube.com/watch*
+// @match    https://www.youtube.com/*
 // ==/UserScript==
-
 function setPlayerSize() {
-  window.eval(
-    `
+  window.eval(`
 _yt_player.Ud = function(a,b){  
   this.width = Math.min(a,722);
   this.height = Math.min(b,408); 
@@ -16,50 +14,55 @@ _yt_player.Ud = function(a,b){
 _yt_player.Ud.prototype.clone = function() {
   return new _yt_player.Ud(this.width,this.height)
 }
-
 _yt_player.Ud.prototype.aspectRatio = function() {
   return this.width / this.height
 }
-
 _yt_player.Ud.prototype.isEmpty = function() {
   return !(this.width * this.height)
 }
-
 _yt_player.Ud.prototype.ceil = function() {
   this.width = Math.ceil(this.width);
   this.height = Math.ceil(this.height);
   return this
 }
-
 _yt_player.Ud.prototype.floor = function() {
   this.width = Math.floor(this.width);
   this.height = Math.floor(this.height);
   return this
 }
-
 _yt_player.Ud.prototype.round = function() {
   this.width = Math.round(this.width);
   this.height = Math.round(this.height);
   return this
 }
-
 window.dispatchEvent(new Event("resize"));
-
 `);
+}
 
+let oldUrl = document.URL;
+let bodyList = document.querySelector("body");
+const observer = new MutationObserver(() => {
+  if (oldUrl != document.URL) {
+    oldUrl = document.URL;
+    exec();
+  }
+});
+observer.observe(bodyList, { childList: true, subtree: true });
+exec();
+
+function exec() {
+  if (document.URL.includes('watch')) {
+    helper("_yt_player.Ud", setPlayerSize);
+    helper("document.getElementById('comments')", autoLoadComments);
+  }
 }
 
 function helper(elem, func) {
-  if (!window.eval(elem)) {
+  if (window && !window.eval(elem)) {
     return setTimeout(() => helper(elem, func), 100);
   }
   func();
 }
-
-setTimeout(() => {
-  helper("_yt_player.Ud", setPlayerSize);
-  helper("document.getElementById('comments')", autoLoadComments);
-}, 100)
 
 // オマケでコメント追加読み込みの自動化
 function autoLoadComments() {
@@ -74,4 +77,3 @@ function autoLoadComments() {
     }
   });
 }
-
